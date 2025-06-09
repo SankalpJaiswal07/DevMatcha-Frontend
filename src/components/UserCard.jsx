822d0ce5b1581e3c5890347cd907e300d5a979cb
SankalpJaiswal07 <sankalpj598@gmail.com>
2025-06-09 20:24:04 +0530

    build user card on feed

---
+++ src/components/UserCard.jsx
@@ -0,0 +1,20 @@
+function UserCard({ user }) {
+  const { firstName, lastName, photoUrl, age, gender, about, skills } = user;
+  return (
+    <div className="card bg-base-300 w-96 shadow-sm">
+      <figure>
+        <img src={photoUrl} alt="Photo" />
+      </figure>
+      <div className="card-body">
+        <h2 className="card-title">{firstName + " " + lastName}</h2>
+        <p>{about}</p>
+        <div className="card-actions justify-center">
+          <button className="btn btn-primary">Ignore</button>
+          <button className="btn btn-secondary">Interested</button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default UserCard;

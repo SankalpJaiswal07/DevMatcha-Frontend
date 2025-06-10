9917d63c086d823415f5048a7d6959e36779341e
SankalpJaiswal07 <sankalpj598@gmail.com>
2025-06-10 20:17:29 +0530

    Import SwipeCard comp

--- src/components/UserCard.jsx
+++ src/components/UserCard.jsx
@@ -1,18 +1,9 @@
-function UserCard({ user }) {
-  const { firstName, lastName, photoUrl, age, gender, about, skills } = user;
+import SwipeCards from "./SwipeCards";
+
+function UserCard({ user, isEdit }) {
   return (
-    <div className="card bg-base-300 w-96 shadow-sm">
-      <figure>
-        <img src={photoUrl} alt="Photo" />
-      </figure>
-      <div className="card-body">
-        <h2 className="card-title">{firstName + " " + lastName}</h2>
-        <p>{about}</p>
-        <div className="card-actions justify-center">
-          <button className="btn btn-primary">Ignore</button>
-          <button className="btn btn-secondary">Interested</button>
-        </div>
-      </div>
+    <div>
+      <SwipeCards user={user} isEdit={isEdit} />
     </div>
   );
 }
